cbf4067580d996f694eddb081323571d9752819f
Lonzo <jmmaglonzo.dev@gmail.com>
2023-10-06 17:52:05 +0800

    finish hero

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,9 +2,19 @@
 module.exports = {
   content: ["./*.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        // Primary
+        strongCyan: " hsl(171, 66%, 44%)",
+        lightBlue: "hsl(233, 100%, 69%)",
+        // Neutral
+        darkGrayishBlue: "hsl(210, 10%, 33%)",
+        grayishBlue: "hsl(201, 11%, 66%)",
+      },
+    },
     container: {
       center: true,
+      padding: "",
     },
   },
   plugins: [],
